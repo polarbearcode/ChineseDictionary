80c543256402a884d434d633f05e0d8ba96d6431
Benny Chen <bechen1@berkeley.edu>
2022-06-01 13:57:17 -0700

    change variables to be Sets instead of Lists

--- ChineseCharacter.java
+++ ChineseCharacter.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /** Represents a Chinese character with simplified and traditional character, audio file,
  * and an example sentence/pairs.
@@ -11,17 +14,20 @@ public class ChineseCharacter {
     /**String for the traditional character. **/
     private String traditional;
 
-    /** String for the Cantonese pronunciation of the character. **/
-    private String cantonesePronunciation;
+    /** Strings for the Cantonese pronunciation of the character. **/
+    private Set<String> cantonesePronunciation;
 
-    /** String for the pinyin pronunciation of the character. **/
-    private String pinyin;
+    /** Strings for the pinyin pronunciation of the character. **/
+    private Set<String> pinyin;
 
-    /** Path to the character's audio in the Audio folder. **/
-    private String audioFile;
+    /** Paths to the character's audio in the Audio folder. **/
+    private Set<String> audioFile;
 
     /** Sentences and/or phrases using the character. **/
-    List<String> exampleUses;
+    private Set<String> exampleUses;
+
+    /** Pronunciation using other Chinese characters. **/
+    private PronunciationAndTone pronunciationWithCharacters;
 
 
     /**
@@ -34,5 +40,64 @@ public class ChineseCharacter {
      */
     public ChineseCharacter(String simplified, String traditional, String cp, String py, String audio) {
 
+        if (checkIfAlreadyAdded(simplified)) {
+            // read in and update;
+        } else {
+
+            this.simplified = simplified;
+            this.traditional = traditional;
+
+            this.cantonesePronunciation = new HashSet<>();
+            this.pinyin = new HashSet<>();
+            this.audioFile = new HashSet<>();
+
+            this.cantonesePronunciation.add(cp);
+            this.pinyin.add(py);
+            this.audioFile.add(audio);
+        }
+
+
+
     }
+
+    /**
+     * Checks if the provided character is already in the dictionary.
+     * @param chineseChar The character to check.
+     * @return  True if the character is already in the dictionary, false otherwise.
+     */
+    private boolean checkIfAlreadyAdded(String chineseChar) {
+        return false;
+    }
+
+    /** Get the simplified character. **/
+    public String getSimplified() {
+        return this.simplified;
+    }
+
+    /** Get the traditional character. **/
+    public String getTraditional() {
+        return this.traditional;
+    }
+
+    /** Get the Cantonese pronunciation(s). **/
+    public Set<String> getCantonesePronunciation() {
+        return this.cantonesePronunciation;
+    }
+
+    /** Get the Mandarin pronunciation(s). **/
+    public Set<String> getMandarinPronunciation() {
+        return this.pinyin;
+    }
+
+    /** Get the example uses list. **/
+    public Set<String> getExampleUses() {
+        return new HashSet<>(this.exampleUses);
+    }
+    
+    /** Get the audio file path. **/
+    public Set<String> getAudioFile() {
+        return this.audioFile;
+    }
+
+
 }

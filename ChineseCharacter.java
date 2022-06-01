c19f29cb0aa00510bbda58616f5f7667130d96d9
Benny Chen <bechen1@berkeley.edu>
2022-05-31 20:11:18 -0700

    initial constructor

---
+++ ChineseCharacter.java
@@ -0,0 +1,25 @@
+/** Represents a Chinese character with simplified and traditional character, audio file,
+ * and an example sentence/pairs.
+ */
+public class ChineseCharacter {
+
+    /** ASCII for simplified character. **/
+    private String simplified;
+
+    /**ASCII for the traditional character. **/
+    private String traditional;
+
+    /** Path to the character's audio in the Audio folder. **/
+    private String audioFile;
+
+
+    /**
+     * Create a Chinese character.
+     * @param s ASCII for simplified character.
+     * @param t ASCII for traditional character.
+     * @param audio A String representing the path to the audio file in the Audio folder.
+     */
+    public ChineseCharacter(String s, String t, String audio) {
+
+    }
+}

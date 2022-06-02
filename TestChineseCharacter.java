86deb4b92a0c6f79c6a172a2d6c061868aa9b6d6
Benny Chen <bechen1@berkeley.edu>
2022-06-02 00:01:07 -0700

    testing adding

--- TestChineseCharacter.java
+++ TestChineseCharacter.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class TestChineseCharacter {
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class TestChineseCharacter {
+
+    @Test
+    public void testNoAudioFile() {
+
+    }
+
+    @Test
+    public void testAlreadyAdded() {
+
+        Set<ChineseCharacter> characterSet = CharacterList.getDictionary();
+
+        ChineseCharacter 汉 = new ChineseCharacter("汉", "漢",
+                "hon3", "han4", "Audio/hon3");
+
+        汉.saveCharacter();
+
+        int curSize = characterSet.size();
+
+        ChineseCharacter copy = new ChineseCharacter("汉", "漢",
+                "hon3", "han4", "Audio/hon3");
+
+        assertEquals(curSize, characterSet.size());
+
+        characterSet.remove(汉);
+
+    }
 }

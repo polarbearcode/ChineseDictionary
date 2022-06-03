b2ee9554ad9bf1c87429511a0d1f5fbec560d875
Benny Chen <bechen1@berkeley.edu>
2022-06-03 11:33:20 -0700

    start using test dictionary

--- TestChineseCharacter.java
+++ TestChineseCharacter.java
@@ -7,17 +7,12 @@ import static org.junit.Assert.*;
 public class TestChineseCharacter {
 
     @Test
-    public void testNoAudioFile() {
-
-    }
-
-    @Test
     public void testAlreadyAdded() {
-
-
 
         ChineseCharacter 汉 = new ChineseCharacter("汉", "漢",
                 "hon3", "han4", "Audio/hon3");
+
+        CharacterList.updateDictionaryFile();
 
         CharacterList.addCharacter(汉);
 
@@ -39,6 +34,20 @@ public class TestChineseCharacter {
         characterSet = CharacterList.getDictionary();
 
         assertEquals(0, characterSet.size());
+    }
 
+    @Test
+    public void testUpdateChar() {
+        ChineseCharacter 汉 = new ChineseCharacter("汉", "漢",
+                "hon3", "han4", "Audio/hon3");
+        CharacterList.addCharacter(汉);
+
+    }
+
+
+    /** Clean up the dictionary after testing. **/
+    private void cleanUpDictionary() {
+        CharacterList.updateDictionaryFile();
+        
     }
 }

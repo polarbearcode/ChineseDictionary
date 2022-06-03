f9288ec3ceae389fdae8ca50d3cfb855da519d52
Benny Chen <bechen1@berkeley.edu>
2022-06-03 15:46:45 -0700

    change test CharacterList now a class

--- TestChineseCharacter.java
+++ TestChineseCharacter.java
@@ -6,48 +6,60 @@ import static org.junit.Assert.*;
 
 public class TestChineseCharacter {
 
+    private final String testDictionaryPath = "./testDictionary.srl";
+    private final CharacterList c = new CharacterList(testDictionaryPath);
+
     @Test
     public void testAlreadyAdded() {
 
         ChineseCharacter 汉 = new ChineseCharacter("汉", "漢",
                 "hon3", "han4", "Audio/hon3");
 
-        CharacterList.updateDictionaryFile();
+        c.addCharacter(汉);
 
-        CharacterList.addCharacter(汉);
-
-        Set<ChineseCharacter> characterSet = CharacterList.getDictionary();
-
-        int curSize = characterSet.size();
+        int curSize = c.size();
 
         ChineseCharacter copy = new ChineseCharacter("汉", "漢",
                 "hon3", "han4", "Audio/hon3");
 
-        CharacterList.addCharacter(copy);
+        c.addCharacter(copy);
 
-        characterSet =CharacterList.getDictionary();
+        assertEquals(curSize, c.size());
 
-        assertEquals(curSize, characterSet.size());
+        c.removeChar("汉");
 
-        CharacterList.removeChar(汉);
 
-        characterSet = CharacterList.getDictionary();
+        assertEquals(0, c.size());
 
-        assertEquals(0, characterSet.size());
+        cleanUpDictionary();
+
+        int x = 10;
     }
 
     @Test
     public void testUpdateChar() {
         ChineseCharacter 汉 = new ChineseCharacter("汉", "漢",
                 "hon3", "han4", "Audio/hon3");
-        CharacterList.addCharacter(汉);
 
+        c.addCharacter(汉);
+
+        ChineseCharacter copy = new ChineseCharacter("汉", "漢",
+                "hon3", "han4", "Audio/hon4");
+
+        c.addCharacter(copy);
+
+        assertEquals(1, c.size());
+
+        assertEquals(2, 汉.getCantonesePronunciation().size());
+
+        cleanUpDictionary();
     }
 
 
     /** Clean up the dictionary after testing. **/
     private void cleanUpDictionary() {
-        CharacterList.updateDictionaryFile();
-        
+        for (String chineseChar : c.getCurrentDictionary().keySet()) {
+            c.removeChar(chineseChar);
+        }
     }
 }

fcb6ff3cf038579c9373f9bafdc4c84f44da4ac8
Benny Chen <bechen1@berkeley.edu>
2022-06-05 16:45:03 -0700

    start writing implementation

--- CantonesePronunciationAndTone.java
+++ CantonesePronunciationAndTone.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class CantonesePronunciationAndTone {
+import java.util.HashMap;
+
+/** Finds the character combo for the Cantonese Pronunciation. **/
+public class CantonesePronunciationAndTone implements PronunciationAndTone {
+
+    /** A HashMap containing the mapping for tones to characters. **/
+    private HashMap<Integer, String> toneCharacters;
+
+    /** A HashMap containing the mapping for pronunciation to characters **/
+    private HashMap<String, String> pronunCharacters;
+
+    /**
+     * Find the character combo for the pronunciation.
+     * @param pronunciation String of the pronunciation in the format of "abcd1"
+     * @return  A length 2 array containing the characters mapping to the pronunciation and the tone.
+     */
+    @Override
+    public String[] characterCombo(String pronunciation) {
+
+    }
 }

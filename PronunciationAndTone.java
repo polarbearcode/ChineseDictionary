b6b75405981f24fcdf2808863ba57ef1d8fc14be
Benny Chen <bechen1@berkeley.edu>
2022-06-06 20:41:45 -0700

    delete unused import

--- PronunciationAndTone.java
+++ PronunciationAndTone.java
@@ -2,6 +2,7 @@ import java.util.List;
 
 /** Stores two characters: one for pronunciation and one for tone. Like 切韵 **/
 public interface PronunciationAndTone {
+
     /**
      * Returns a combination of characters that equal to the pronunciation of the provided character.
      * @param chineseCharacter The character to find the pronunciation for.

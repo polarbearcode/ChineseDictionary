880e4a8b1b1b5ec570b69de3f2a1293b7e42915b
Benny Chen <bechen1@berkeley.edu>
2022-06-11 00:39:38 -0700

    add elements programatically

--- CharacterInput.java
+++ CharacterInput.java
@@ -1,15 +1,73 @@
 import javax.swing.*;
+import java.util.List;
 
-import java.awt.*;
+import java.awt.Font;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CharacterInput {
 
+    /** Font for the text box labels on the left side. **/
+    private final Font labelFont = new Font("Comic Sans MS", Font.BOLD, 16);
+
+    /** The width of the GUI window. **/
+    private int frameWidth;
+
+    /** The height of the GUI windows. **/
+    private int frameHeight;
+
+    /** The JFrame for the character input. **/
+    private JFrame mainFrame;
+
+    /** Save the JTextAreas to get the information. **/
+    private List<JTextArea> inputTextList;
+
+    /**
+     * Add the 5 required text boxes and their labels (simplified char, traditional char, cantonese pronunciation,
+     * pinyin, and examples to the main frame of this.
+     */
+    private void addFiveTextBoxes() {
+        double marginPercentage = 0.1;
+        double labelWidthProportion = 0.25;
+        double labelHeightProportion = 0.1;
+        double inputWidthProportion = 0.5;
+        double inputHeightProportion = 0.1;
+
+
+        int topAndBotMargin =  (int) (this.frameHeight * marginPercentage);
+        int leftAndRightMargin = (int) (this.frameWidth * marginPercentage);
+
+        int startY = frameHeight - topAndBotMargin;
+        int startX = leftAndRightMargin;
+
+        int labelWidth = (int)(this.frameWidth * labelWidthProportion);
+        int labelHeight = (int) (this.frameHeight * labelHeightProportion);
+
+        int inputWidth = (int)(this.frameWidth * inputWidthProportion);
+        int inputHeight = (int)(this.frameHeight * inputHeightProportion);
+
+        String[] labelStrings = new String[]{"Simplified Character", "Traditional Character"
+                ,"Cantonese Pronunciations", "Pinyin", "Examples"};
+
+        for (int i = 0; i < labelStrings.length; i = i + 1) {
+            JLabel label = new JLabel(labelStrings[i]);
+            label.setFont(this.labelFont);
+            int curHeight = startY - (10 * i);
+            label.setBounds(leftAndRightMargin, curHeight, labelWidth, labelHeight);
+
+            JTextArea textInput = new JTextArea();
+            textInput.setBounds(leftAndRightMargin + labelWidth, curHeight, inputWidth, inputHeight);
+
+            this.inputTextList.add(textInput);
+
+            
+        }
+    }
+
+
 
     public static void main(String[] args) {
-
-
 
         JFrame mainFrame = new JFrame("Character Input");
         mainFrame.setSize(400,400);

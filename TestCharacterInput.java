fce5e93b17c52f5f294cb9ea29b3fde07de7478a
Benny Chen <bechen1@berkeley.edu>
2022-06-15 19:26:25 -0700

    assertj swing testing

--- TestCharacterInput.java
+++ TestCharacterInput.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class TestCharacterInput {
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.assertj.swing.fixture.FrameFixture;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCharacterInput {
+    private FrameFixture window;
+
+    @Before
+    public void setUp() {
+        CharacterList testCharList = new CharacterList("./testdictionary.srl");
+        CharacterInput c = GuiActionRunner.execute(() -> new CharacterInput(500, 500, testCharList));
+        window = new FrameFixture(c.getMainFrame());
+        window.show();
+    }
+
+    @Test
+    public void testCharacterInput() {
+
+        window.textBox("Simplified Character").enterText("种");
+        window.textBox("Traditional Character").enterText("種");
+        window.textBox("Pinyin").enterText("zhong3");
+        window.textBox("Cantonese Pronunciations").enterText("zung2, zung3");
+        window.textBox("Examples").enterText("种植,种群");
+        window.button("Add Button").click();
+
+    }
 }

ba1d801ba3033a6da703d7e35eb06c69432382ec
Benny Chen <bechen1@berkeley.edu>
2022-06-18 13:19:06 -0700

    complete example checker

--- InputCheckerExamples.java
+++ InputCheckerExamples.java
@@ -1,3 +1,26 @@
-/** Check inputted examples. **/
-public class InputCheckerExamples {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/** Check entered examples. **/
+public class InputCheckerExamples implements InputChecker {
+
+    /**
+     * Check the entered example is valid.
+     * @param input The example entered by the user
+     * @param chineseCharToContain  The character the example should contain.
+     * @return  True if the example contains the character and is in the right format, false otherwise.
+     */
+    @Override
+    public boolean checkInput(String input, String chineseCharToContain) {
+
+        if (!input.contains(chineseCharToContain)) {
+            return false;
+        }
+
+        String patternString = Utils.addRegexCarrotAndDollarSign(Utils.examplePatternString);
+        Matcher m = Pattern.compile(patternString).matcher(input);
+
+        return m.find();
+    }
+
 }

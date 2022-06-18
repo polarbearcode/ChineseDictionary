68dea1374f4f304e01f94e4c1da6c4c41aafd41e
Benny Chen <bechen1@berkeley.edu>
2022-06-18 13:01:47 -0700

    interfaces for checking inputs

--- GetCantonesePronunciation.java
+++ GetCantonesePronunciation.java
@@ -1,15 +1,29 @@
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /** Get the Cantonese pronunciation of a character.  **/
-public class GetCantonesePronunciation implements GetPronunciationLanguage{
+public class GetCantonesePronunciation implements GetPronunciationLanguage, InputChecker{
 
     /**
      * Get the Cantonese pronunciation of the character.
-     * @param chineseChar
-     * @return
+     * @param chineseChar The ChineseCharacter to get the Cantonese pronunciation of.
+     * @return  The Set of Cantonese pronunciations for the character.
      */
     @Override
     public Set<String> getPronunciation(ChineseCharacter chineseChar) {
         return chineseChar.getCantonesePronunciation();
+    }
+
+    /**
+     * Check the input that the user entered is in the right format.
+     * @param input The pronunciation the user entered.
+     * @return  True if the input is in the right format, false otherwise.
+     */
+    @Override
+    public boolean checkInput(String input) {
+        String patternString = Utils.addRegexCarrotAndDollarSign(Utils.cantonesePronunciationPatternString);
+        Matcher m = Pattern.compile(patternString).matcher(input);
+        return m.find();
     }
 }
--- GetMandarinPronunciation.java
+++ GetMandarinPronunciation.java
@@ -1,10 +1,24 @@
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /** Get the pinyin of a character.  **/
-public class GetMandarinPronunciation implements GetPronunciationLanguage {
+public class GetMandarinPronunciation implements GetPronunciationLanguage, InputChecker {
 
     @Override
     public Set<String> getPronunciation(ChineseCharacter chineseChar) {
         return chineseChar.getMandarinPronunciation();
+    }
+
+    /**
+     * Check the input that the user entered is in the right format.
+     * @param input The pronunciation the user entered.
+     * @return  True if the input is in the right format, false otherwise.
+     */
+    @Override
+    public boolean checkInput(String input) {
+        String patternString = Utils.addRegexCarrotAndDollarSign(Utils.pinyinPronunciationPatternString);
+        Matcher m = Pattern.compile(patternString).matcher(input);
+        return m.find();
     }
 }

3b625ad51bda6f0d52917775ee3a9a0fc8e10da4
Benny Chen <bechen1@berkeley.edu>
2022-06-18 13:03:16 -0700

    add some variables to reduce code repitition

--- Utils.java
+++ Utils.java
@@ -1,8 +1,22 @@
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.regex.Pattern;
 
-public class WriteFile {
+public class Utils {
+
+    /** String to use for cantonesePronunciationPattern. **/
+    public static final String cantonesePronunciationPatternString = "([a-zA-Z]+)([1-6])";
+
+    /** String to use for mandarinPronunciationPattern. **/
+    public static final String pinyinPronunciationPatternString = "([a-zA-Z]+)([1-4]?)";
+
+
+    /** Pattern to check Cantonese pronunciations. **/
+    public static final Pattern cantonesePronunciationPattern = Pattern.compile(cantonesePronunciationPatternString);
+
+    /** Pattern to check Pinyin. **/
+    public static final Pattern mandarinPronunciationPattern = Pattern.compile(pinyinPronunciationPatternString);
 
     /**
      * Write the object to the file specified by file path.
@@ -19,4 +33,14 @@ public class WriteFile {
             io.printStackTrace();
         }
     }
+
+    /**
+     * Add ^ and $ to the pattern string so that it becomes ^patternString$ for checking inputs.
+     * @param patternString The pattern string to change.
+     * @return  Return a String in the format of  ^patternString$
+     */
+    public static String addRegexCarrotAndDollarSign(String patternString) {
+        return "^" + patternString + "$";
+    }
+
 }

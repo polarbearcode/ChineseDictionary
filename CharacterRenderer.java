a012284230594031eee043251e5447b67b846c06
Benny Chen <bechen1@berkeley.edu>
2022-06-20 20:57:15 -0700

    add keyboard commands, still need to fix

--- CharacterRenderer.java
+++ CharacterRenderer.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.introcs.StdDraw;
+
 import java.awt.*;
 
 /** Put the character information onto the window. **/
@@ -10,8 +12,7 @@ public class CharacterRenderer {
 
     private final int windowHeight = 300;
 
-
-
+    private boolean showScreen = true;
 
 
     /**
@@ -20,21 +21,36 @@ public class CharacterRenderer {
      */
     public CharacterRenderer(ChineseCharacter chineseChar) {
         this.chineseChar = chineseChar;
-
         StdDraw.setCanvasSize(this.windowWidth, this.windowHeight);
-        StdDraw.clear(Color.ORANGE);
-
+        StdDraw.enableDoubleBuffering();
         drawToScreen();
+
     }
 
     public void drawToScreen() {
-        
-        StdDraw.setPenColor(Color.BLACK);
-        Font mainFont = new Font("Open Sans, Lucida Sans", Font.PLAIN, 50);
-        StdDraw.setFont(mainFont);
-        StdDraw.text(0.3, 0.6, this.chineseChar.getSimplified());
-        StdDraw.text(0.6, 0.6, this.chineseChar.getTraditional());
-        StdDraw.show();
 
-    }
+            boolean showPronunciation = true;
+
+            if (StdDraw.hasNextKeyTyped()) {
+                char command = StdDraw.nextKeyTyped();
+                if (command == 'h') {
+                    showPronunciation = false;
+                } else if (command == 'm') {
+                    this.showScreen = false;
+                    return;
+                }
+            }
+
+            StdDraw.clear(Color.ORANGE);
+
+            StdDraw.setPenColor(Color.BLACK);
+            Font mainFont = new Font("Open Sans, Lucida Sans", Font.PLAIN, 50);
+            StdDraw.setFont(mainFont);
+            StdDraw.text(0.3, 0.8, this.chineseChar.getSimplified());
+            StdDraw.text(0.6, 0.8, this.chineseChar.getTraditional());
+            Font englishFont = new Font("Comic Sans MS", Font.PLAIN, 20);
+            StdDraw.setFont(englishFont);
+            StdDraw.text(0.2, 0.2, "Examples");
+            StdDraw.show();
+        }
 }

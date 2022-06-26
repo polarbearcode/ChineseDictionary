fd191249ad78e7e195c8443fc5c06c80f8d1f197
Benny Chen <bechen1@berkeley.edu>
2022-06-26 09:28:43 -0700

    pronunciations on screen

--- CharacterRenderer.java
+++ CharacterRenderer.java
@@ -1,6 +1,9 @@
 import edu.princeton.cs.introcs.StdDraw;
+import java.awt.Color;
+import java.awt.Font;
+import java.util.List;
+import java.util.Set;
 
-import java.awt.*;
 
 /** Put the character information onto the window. **/
 public class CharacterRenderer implements DictionaryScreen {
@@ -12,9 +15,14 @@ public class CharacterRenderer implements DictionaryScreen {
 
     private final int windowHeight = 300;
 
+
     private boolean showScreen = true;
 
     private boolean showPronunciation = true;
+
+    private final String cantoFinderMapPath = "./testCPAndT.srl";
+
+    private final String mFinderMapPath = "./testMPAndT.srl";
 
     CharacterRenderer(ChineseCharacter chineseChar) {
         this.chineseChar = chineseChar;
@@ -26,6 +34,11 @@ public class CharacterRenderer implements DictionaryScreen {
      */
     @Override
     public void drawToScreen() {
+
+        double simplifiedSide = 0.3;
+        double traditionalSide = 0.6;
+        double characterDrawY = 0.8;
+
         while (true) {
                 if (StdDraw.hasNextKeyTyped()) {
                     char command = StdDraw.nextKeyTyped();
@@ -41,11 +54,21 @@ public class CharacterRenderer implements DictionaryScreen {
 
                 StdDraw.setPenColor(Color.BLACK);
                 StdDraw.setFont(Utils.createChineseFont(50));
-                StdDraw.text(0.3, 0.8, chineseChar.getSimplified());
-                StdDraw.text(0.6, 0.8, chineseChar.getTraditional());
+
+                StdDraw.text(simplifiedSide, characterDrawY, chineseChar.getSimplified());
+                StdDraw.text(traditionalSide, characterDrawY, chineseChar.getTraditional());
+
+                if (showPronunciation) {
+                    CantoPAndT cantoFinder = new CantoPAndT(this.cantoFinderMapPath);
+                    MPAndT mFinder = new MPAndT(this.mFinderMapPath);
+
+                    drawPronunciation(cantoFinder.characterCombo(this.chineseChar), traditionalSide);
+                    drawPronunciation(mFinder.characterCombo(this.chineseChar), simplifiedSide);
+                }
+
                 Font englishFont = new Font("Comic Sans MS", Font.PLAIN, 20);
                 StdDraw.setFont(englishFont);
-                StdDraw.text(0.2, 0.5, "Examples");
+                StdDraw.text(0.2, 0.3, "Examples");
 
                 StdDraw.setFont(Utils.createChineseFont(20));
                 int i = 0;
@@ -61,5 +84,25 @@ public class CharacterRenderer implements DictionaryScreen {
                 }
             }
         }
+
+    /**
+     * Put the pronunciation combos of a character to the screen.
+     * @param pronunciations    List of pronunciation combos from the Character.
+     * @param xCoord    The coordinate that the character is drawn to. Use to find relative position
+     *                  to place the pronunciations.
+     */
+    private void drawPronunciation(List<String[]> pronunciations, double xCoord) {
+
+        StdDraw.setFont(Utils.createChineseFont(20));
+
+        double yStart = 0.6;
+        int ySubtract = 0;
+        for (String[] pronunciation : pronunciations) {
+            StdDraw.text(xCoord, yStart - (0.05 * ySubtract),
+                    pronunciation[0] + pronunciation[1]);
+            ySubtract = ySubtract + 1;
+        }
+
+    }
 
     }

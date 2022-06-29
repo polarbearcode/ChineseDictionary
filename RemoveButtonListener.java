d3014e2b292cd7f7b88fd0580bb67145ab6e5b40
Benny Chen <bechen1@berkeley.edu>
2022-06-29 14:07:49 -0700

    add javadocs

--- RemoveButtonListener.java
+++ RemoveButtonListener.java
@@ -1,10 +1,19 @@
 import java.awt.event.ActionListener;
 
+/** Listener for the Remove Button on the edit character screen. **/
 public class RemoveButtonListener extends EditScreenButtons implements ActionListener {
 
+    /**
+     * Tie button to the EditCharacter screen.
+     * @param editScreen    The EditCharacter screen to get the character input.
+     */
     RemoveButtonListener(LookUpCharacter editScreen) {
         super(editScreen);
     }
+
+    /**
+     * Remove the character from the CharacterList. 
+     */
     @Override
     public void action() {
         this.editScreen.getCharList().removeChar(this.editScreen.getCharBox().getText());

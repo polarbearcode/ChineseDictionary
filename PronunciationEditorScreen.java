81e8d834e960d8df9faa15a08c1832fa703ff8fa
Benny Chen <bechen1@berkeley.edu>
2022-06-30 20:20:43 -0700

    start options for screen

--- PronunciationEditorScreen.java
+++ PronunciationEditorScreen.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class PronunciationEditorScreen {
+import edu.princeton.cs.introcs.StdDraw;
+
+import java.awt.*;
+
+/** Screen with edit pronunciation options for tone character or pronunciation character mappings. **/
+public class PronunciationEditorScreen {
+
+    private void mainMenu() {
+        StdDraw.clear(Color.orange);
+        StdDraw.setFont(Utils.comicSansFont(20));
+        StdDraw.text(0.5, 0.5, "Edit Tone Mapping (T)");
+        StdDraw.text(0.5, 0.4, "Edit Pronunciation Mapping (P)");
+        StdDraw.text(0.5, 0.3, "Main Menu (M)");
+    }
+
+    public static void drawToScreen() {
+        while (true) {
+
+            if (StdDraw.hasNextKeyTyped()) {
+                if (StdDraw.nextKeyTyped() == 't') {
+                    // tone editor GUI
+                } else if (StdDraw.nextKeyTyped() == 'p') {
+                    // pronunciation editor GUI
+                } else if (StdDraw.nextKeyTyped() == 'm') {
+                    // back to main menu
+                }
+            }
+
+        }
+    }
+
+
 }

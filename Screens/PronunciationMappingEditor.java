bb260b9d4eee21b48fa8e41e76e91e8d671a79bb
polarbearcode <bechen1@berkeley.edu>
2022-07-03 11:08:58 -0700

    add jtextarea

--- Screens/PronunciationMappingEditor.java
+++ Screens/PronunciationMappingEditor.java
@@ -1,11 +1,25 @@
 package Screens;
 
 import Characters.PronunciationAndTone;
+import Utils.Utils;
+
+import javax.swing.*;
+import java.util.Map;
 
 /** Screen that displays pronunciation -> character mapping and allows user to edit it. **/
 public class PronunciationMappingEditor implements DictionaryScreen{
 
-    PronunciationAndTone p;
+    /** Contain the mapping information. **/
+    private PronunciationAndTone p;
+
+    /** The main frame of the GUI. **/
+    private JFrame mainFrame;
+
+    /** Height of the main frame window. **/
+    private final int windowHeight = 500;
+
+    /** Width of the main frame window. **/
+    private final int windowWidth = 500;
 
     /**
      * Instantiate a PronunciationEditorScreen either for Cantonese or Mandarin mappings.
@@ -13,11 +27,21 @@ public class PronunciationMappingEditor implements DictionaryScreen{
      */
     public PronunciationMappingEditor(PronunciationAndTone p) {
         this.p = p;
+        this.mainFrame = new JFrame("Edit Pronunciation Mapping");
+        this.mainFrame.setSize(this.windowWidth, this.windowHeight);
     }
 
     @Override
     public void drawToScreen() {
-        
+        JLabel labelPronunciation = new JLabel("Pronunciation");
+        JLabel characterMap = new JLabel("Character");
+
+        Utils.setJComponentBounds(labelPronunciation, 0.3, 0.5, 0.3, 0.3,
+                this.windowWidth, this.windowHeight);
+
+
+
+
     }
 
 

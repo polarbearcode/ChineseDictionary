e944d7154aae4759587fcc772cc26a8f334fa7b9
polarbearcode <bechen1@berkeley.edu>
2022-07-03 10:44:46 -0700

    start editor screens

--- Screens/PronunciationMappingEditor.java
+++ Screens/PronunciationMappingEditor.java
@@ -1,2 +1,24 @@
-package Screens;public class PronunciationMappingEditor {
+package Screens;
+
+import Characters.PronunciationAndTone;
+
+/** Screen that displays pronunciation -> character mapping and allows user to edit it. **/
+public class PronunciationMappingEditor implements DictionaryScreen{
+
+    PronunciationAndTone p;
+
+    /**
+     * Instantiate a PronunciationEditorScreen either for Cantonese or Mandarin mappings.
+     * @param p A PronunciationAndTone object to get the mapping information for the scree.
+     */
+    public PronunciationMappingEditor(PronunciationAndTone p) {
+        this.p = p;
+    }
+
+    @Override
+    public void drawToScreen() {
+        
+    }
+
+
 }

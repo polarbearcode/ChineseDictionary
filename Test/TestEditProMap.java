6d9ce207158c7b539deb80e8343b0bda602406b3
polarbearcode <bechen1@berkeley.edu>
2022-07-04 10:00:42 -0700

    write test

--- Test/TestEditProMap.java
+++ Test/TestEditProMap.java
@@ -5,23 +5,46 @@ import org.assertj.swing.fixture.FrameFixture;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import Characters.*;
 import Screens.*;
 
 public class TestEditProMap {
 
-    private FrameFixture window;
+    private FrameFixture cantoneseEditor;
+    CantoPAndT cantoPAndT;
+
 
     @Before
     public void setUp() {
 
-        CantoPAndT cantoPAndT = new CantoPAndT(Start.getPathToCantoPAndT());
+        cantoPAndT = new CantoPAndT(Start.getPathToCantoPAndT());
+
+        cantoPAndT.clearMapping();
+
+        cantoPAndT.addPronunciationMapping("zung", "种");
 
         PronunciationMappingEditor pME = GuiActionRunner.execute(() -> new PronunciationMappingEditor(cantoPAndT));
 
-        window = new FrameFixture(pME.getMainFrame());
-        window.show();
-        window.maximize();
+        pME.drawToScreen();
+
+        cantoneseEditor = new FrameFixture(pME.getMainFrame());
+        cantoneseEditor.show();
+        cantoneseEditor.maximize();
+    }
+
+    @Test
+    public void testCantoneseEditor() {
+        cantoneseEditor.textBox("Pronunciation").setText("zung");
+        cantoneseEditor.textBox("Character").setText("中");
+        cantoneseEditor.button("Edit").click();
+
+        assertEquals("中", cantoPAndT.getPronunCharacters().get("zung"));
+    }
+
+    @After
+    public void tearDown() {
+        cantoneseEditor.cleanUp();
     }
 }

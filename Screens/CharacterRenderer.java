a3f427078757607e7be78ce1ac1013d762c7a4cf
polarbearcode <bechen1@berkeley.edu>
2022-07-05 16:21:12 -0700

    add pronunciation numbers

--- Screens/CharacterRenderer.java
+++ Screens/CharacterRenderer.java
@@ -106,10 +106,13 @@ public class CharacterRenderer implements DictionaryScreen {
 
         double yStart = 0.6;
         int ySubtract = 0;
+
+        int count = 1;
         for (String[] pronunciation : pronunciations) {
             StdDraw.text(xCoord, yStart - (0.05 * ySubtract),
-                    pronunciation[0] + pronunciation[1]);
+                    pronunciation[0] + pronunciation[1] + " " + count);
             ySubtract = ySubtract + 1;
+            count = count + 1;
         }
 
     }

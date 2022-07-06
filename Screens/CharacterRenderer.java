4ae6a929d12ee4603ae85a0136b88521219904d0
polarbearcode <bechen1@berkeley.edu>
2022-07-06 16:19:57 -0700

    use helper functions to draw

--- Screens/CharacterRenderer.java
+++ Screens/CharacterRenderer.java
@@ -42,28 +42,24 @@ public class CharacterRenderer implements DictionaryScreen {
         double traditionalSide = 0.6;
         double characterDrawY = 0.8;
 
+        this.drawTopScreen(simplifiedSide, traditionalSide, characterDrawY);
+        this.drawBotScreen();
+
         while (true) {
-                if (StdDraw.hasNextKeyTyped()) {
-                    char command = StdDraw.nextKeyTyped();
-                    if (command == 'h') {
-                        showPronunciation = !showPronunciation;
-                    } else if (command == 'm') {
-                        Start.mainMenu();
-                        showScreen = false;
-                        return;
-                    } else if (command == 'q') {
-                        Start.setShowScreen(false);
-                        return;
-                    }
+
+                char command = Utils.getNextCommand();
+
+                this.drawTopScreen(simplifiedSide, traditionalSide, characterDrawY);
+
+                if (command == 'h') {
+                    showPronunciation = !showPronunciation;
+                } else if (command == 'm') {
+                    showScreen = false;
+                    return;
+                } else if (command == 'q') {
+                    Start.setShowScreen(false);
+                    return;
                 }
-
-                StdDraw.clear(Color.ORANGE);
-
-                StdDraw.setPenColor(Color.BLACK);
-                StdDraw.setFont(Utils.createChineseFont(50));
-
-                StdDraw.text(simplifiedSide, characterDrawY, chineseChar.getSimplified());
-                StdDraw.text(traditionalSide, characterDrawY, chineseChar.getTraditional());
 
                 if (showPronunciation) {
                     CantoPAndT cantoFinder = new CantoPAndT(Start.getPathToCantoPAndT());
@@ -73,22 +69,9 @@ public class CharacterRenderer implements DictionaryScreen {
                     drawPronunciation(mFinder.characterCombo(this.chineseChar), simplifiedSide);
                 }
 
-                Font englishFont = Utils.comicSansFont(20);
-                StdDraw.setFont(englishFont);
-                StdDraw.text(0.2, 0.4, "Examples");
-
-                StdDraw.setFont(Utils.createChineseFont(20));
-                int i = 0;
-                for (String example : chineseChar.getExampleUses()) {
-                    StdDraw.text(0.2, 0.3 - (0.05 * i), example);
-                    i = i + 1;
-                }
+                this.drawBotScreen();
 
                 StdDraw.show();
-
-                while (!StdDraw.hasNextKeyTyped()) {
-                    continue;
-                }
             }
         }
 
@@ -115,4 +98,36 @@ public class CharacterRenderer implements DictionaryScreen {
 
     }
 
+    /**
+     * Draws the part of the screen above the pronunciations.
+     * @param simplifiedSide    double, determines where to put the simplified character.
+     * @param traditionalSide   double, determines where to put the traditional character.
+     * @param characterDrawY    double, where vertically to draw both characters
+     */
+    private void drawTopScreen(double simplifiedSide, double traditionalSide, double characterDrawY) {
+        StdDraw.clear(Color.ORANGE);
+
+        StdDraw.setPenColor(Color.BLACK);
+        StdDraw.setFont(Utils.createChineseFont(50));
+
+        StdDraw.text(simplifiedSide, characterDrawY, chineseChar.getSimplified());
+        StdDraw.text(traditionalSide, characterDrawY, chineseChar.getTraditional());
     }
+
+    /**
+     * Draw the part of the screen below the pronunciations.
+     */
+    private void drawBotScreen() {
+        Font englishFont = Utils.comicSansFont(20);
+        StdDraw.setFont(englishFont);
+        StdDraw.text(0.2, 0.4, "Examples");
+
+        StdDraw.setFont(Utils.createChineseFont(20));
+        int i = 0;
+        for (String example : chineseChar.getExampleUses()) {
+            StdDraw.text(0.2, 0.3 - (0.05 * i), example);
+            i = i + 1;
+        }
+    }
+
+}

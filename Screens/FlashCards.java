c472fbc58bdd688f9a6a4939b1ab5b9c6e2be433
polarbearcode <bechen1@berkeley.edu>
2022-07-08 20:36:48 -0700

    start add instance variable and nested class for list to store chars

--- Screens/FlashCards.java
+++ Screens/FlashCards.java
@@ -1,5 +1,31 @@
+package Screens;
 
-/** The actual display for the flash cards. **/
-public class FlashCards {
+import Characters.CharacterList;
+
+/** The actual display for the flash cards. User can use arrows to go through the characters. **/
+public class FlashCards implements DictionaryScreen {
+
+    CharacterList charList;
+
+    /** The characters in the charlist as a list. **/
+
+
+    /**
+     * Instantiate a Flash Card Screen tied to the character list.
+     * @param charList  The CharacterList containing the characters to display.
+     */
+    public FlashCards(CharacterList charList) {
+        this.charList = charList;
+    }
+
+    @Override
+    public void drawToScreen() {
+
+    }
+
+    /** Use to put characters in a list and cycle through them. **/
+    private static class CharLinkedList {
+
+    }
 
 }

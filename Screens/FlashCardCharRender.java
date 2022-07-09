4482a98ae0bd2e55e1baeabe8e0ffac5952b2387
polarbearcode <bechen1@berkeley.edu>
2022-07-08 20:37:19 -0700

    start adding process command

--- Screens/FlashCardCharRender.java
+++ Screens/FlashCardCharRender.java
@@ -1,2 +1,43 @@
-package Screens;public class FlashCardCharRender {
+package Screens;
+
+import Characters.CharacterList;
+import Characters.ChineseCharacter;
+
+/** A Character Renderer specifically used for the flash cards. Has an extra command for
+ * shuffling through the characters.
+ */
+public class FlashCardCharRender extends CharacterRenderer{
+
+    /** The character list **/
+    private CharacterList charList;
+
+    /**
+     * Instantiate a Character Renderer for the Chinese character tied to the character list.
+     * @param chineseChar   The character to display on the current screen.
+     * @param charList  The Character List the character is tied to.
+     */
+    public FlashCardCharRender(ChineseCharacter chineseChar, CharacterList charList) {
+        super(chineseChar);
+        this.charList = charList;
+    }
+
+    /**
+     * Process the user command.
+     * @param command   The char entered.
+     */
+    private void processCommand(char command){
+        if (command == 'h') {
+            this.setShowScreen(!this.getShowScreen());
+        } else if (command == 'm') {
+            this.setShowScreen(false);
+            return;
+        } else if (command == 'q') {
+            Start.setShowScreen(false);
+            return;
+        } else if ( this.getShowScreen() && this.getPronunciationCommands().containsKey(command)) {
+            this.playFile(command);
+        } else if (command == 'n') {
+
+        }
+    }
 }
